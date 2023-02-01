0299a25c9c03e5db935d12f29c27ab20511908a5
thards89 <thards89@gmail.com>
2023-02-01 17:12:11 +0100

    server updtated

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const apiUrl =
-  process.env.API_URL || "https://manga-managment-app.herokuapp.com";
+  process.env.API_URL || "https://manga-app-backend-production.up.railway.app";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
