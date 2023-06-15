743302b5bc75550c576e2d81ceb5c1dd764704e5
thards89 <thards89@gmail.com>
2023-06-15 19:34:14 +0100

    localhost access set up

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,4 @@
 export const apiUrl =
-  process.env.API_URL || "manga-app-backend-production-5fe0.up.railway.app";
+  // process.env.API_URL || "manga-app-backend-production-5fe0.up.railway.app";
+  "http://localhost:4000"
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
